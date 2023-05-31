4d0212e68813f47e54be1fde15a83987d63ac3db
Md Nazmul Islam <55467254+devnazmul@users.noreply.github.com>
2023-05-31 10:25:49 +0600

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get(`/`, (req: Request, res: Response) => {
-  res.send('server is running.');
+  res.send('server is running....');
 });
 
 export default app;
