1530eb847a3a679f9519320be6dcb8e401e30c9a
Md Nazmul Islam <55467254+devnazmul@users.noreply.github.com>
2023-05-31 09:40:22 +0600

    [DONE] Setup express, mongoose, cors, Parser

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import cors from 'cors';
+import express, { Application, Request, Response } from 'express';
+
+const app: Application = express();
+
+// MIDDLEWARE
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get(`/`, (req: Request, res: Response) => {
+  res.send('server is running.');
+});
+
+export default app;
